18b801bdd3676bb2e688f494975249854be31e85
SergioMEspinoza <sergio.espinoza.or@gmail.com>
2026-02-03 22:01:52 -0400

    segundo cambio

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,15 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { suma } from '../src/suma.js'
 
-test('sumar 7 + 11 es igual a 18', () => {
-  expect(suma(7, 11)).toBe(18)
+
+describe('sum', () =>{
+
+    it('sumar 7 + 11 es igual a 18', () => {
+    expect(suma(7, 11)).toBe(18)
+    })
+
+    it('sumar 2 + 2 es igual a 4', () => {
+    expect(suma(2, 2)).toBe(4)
+    })
+
 })
